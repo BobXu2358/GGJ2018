20b5063e33d26ad7f729e838e834d910cd860cb7
Xinhai Xu <xxh2358@gmail.com>
2018-01-26 20:39:27 +0800

    bullet stuff

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            Destroy(col.gameObject);
+            this.gameObject.GetComponent<PlayerAction>().enabled = false;
+            col.gameObject.GetComponent<PlayerAction>().enabled = true;
+        }
+    }
+}
