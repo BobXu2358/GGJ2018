943dc4c0a77e1d9635076bf3a80f3ee217b7e58f
catsworld <519418441@qq.com>
2018-01-26 20:05:46 +0800

    Init simple player action controller.

--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -1,6 +1,26 @@
 ﻿using UnityEngine;
 
 public class PlayerAction : MonoBehaviour {
+    public Transform playerTf;
+    public Rigidbody playerRb;
+    public float moveSpeed;
+    public float jumpForce;
+    bool grounded = true;
+
     void FixedUpdate(){
+        Vector3 moveOffset = new Vector3(0, 0, 0);
+        moveOffset.x += Input.GetAxisRaw("Horizontal") * moveSpeed;
+        playerTf.position += moveOffset;
+
+        if(Input.GetButton("Jump") && grounded){
+            grounded = false;
+            playerRb.AddForce(0, jumpForce, 0);
+        }
+    }
+
+    void OnCollisionEnter(Collision collisionObject){
+        if(collisionObject.gameObject != gameObject){
+            grounded = true;
+        }
     }
 }
