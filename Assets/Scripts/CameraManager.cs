85d9fd8803e31689918f25ef62aeb6bcf62a2b27
catsworld <519418441@qq.com>
2018-01-26 22:12:39 +0800

    Complete some systems

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -3,15 +3,25 @@
 public class CameraManager : MonoBehaviour {
     public Transform followingTf;
     public Transform cameraTf;
-    public Transform groundTf;
+    public Transform mapGroundTf;
+    public Camera camera;
     public Vector3 cameraOffset;
+    float cameraHalfWidth;
+    float cameraHalfHeight;
+
+    void Start(){
+        cameraHalfHeight = camera.orthographicSize;
+        cameraHalfWidth = cameraHalfHeight / Screen.height * Screen.width;
+    }
 
     void Update(){
         Vector3 finCameraPos = followingTf.position + cameraOffset;
-        if(finCameraPos.x - 12.5 <= groundTf.position.x - groundTf.lossyScale.x / 2)
-            finCameraPos.x = groundTf.position.x - groundTf.lossyScale.x / 2 + (float)12.5;
-        if(finCameraPos.x + 12.5 >= groundTf.position.x + groundTf.lossyScale.x / 2)
-            finCameraPos.x = groundTf.position.x + groundTf.lossyScale.x / 2 - (float)12.5;
+        if(finCameraPos.x - cameraHalfWidth <= mapGroundTf.position.x - mapGroundTf.lossyScale.x / 2)
+            finCameraPos.x = mapGroundTf.position.x - mapGroundTf.lossyScale.x / 2 + cameraHalfWidth;
+        if(finCameraPos.x + cameraHalfWidth >= mapGroundTf.position.x + mapGroundTf.lossyScale.x / 2)
+            finCameraPos.x = mapGroundTf.position.x + mapGroundTf.lossyScale.x / 2 - cameraHalfWidth;
+        if(finCameraPos.y - cameraHalfHeight <= mapGroundTf.position.y + mapGroundTf.lossyScale.y / 2 + followingTf.lossyScale.y)
+            finCameraPos.y = mapGroundTf.position.y + mapGroundTf.lossyScale.y / 2 + followingTf.lossyScale.y + cameraHalfHeight;
         cameraTf.position = finCameraPos;
     }
 }
--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerAction : MonoBehaviour {
     public Transform playerTf;
     public Rigidbody playerRb;
+    public Transform mapGroundTf;
     public float moveSpeed;
     public float jumpForce;
     bool grounded = true;
@@ -10,9 +12,19 @@ public class PlayerAction : MonoBehaviour {
     void FixedUpdate(){
         Vector3 moveOffset = new Vector3(0, 0, 0);
         moveOffset.x += Input.GetAxisRaw("Horizontal") * moveSpeed;
-        playerTf.position += moveOffset;
+        Vector3 finPlayerPos = playerTf.position + moveOffset;
 
-        if(Input.GetButtonDown("Jump") && grounded){
+        if(finPlayerPos.x - playerTf.lossyScale.x / 2 <= mapGroundTf.position.x - mapGroundTf.lossyScale.x / 2)
+            finPlayerPos.x = mapGroundTf.position.x - mapGroundTf.lossyScale.x / 2 + playerTf.lossyScale.x / 2;
+        if(finPlayerPos.x + playerTf.lossyScale.x / 2 >= mapGroundTf.position.x + mapGroundTf.lossyScale.x / 2)
+            finPlayerPos.x = mapGroundTf.position.x + mapGroundTf.lossyScale.x / 2 - playerTf.lossyScale.x / 2;
+
+        playerTf.position = finPlayerPos;
+
+        if(playerTf.position.y - playerTf.lossyScale.y / 2 <= mapGroundTf.position.y + mapGroundTf.lossyScale.y / 2)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        if(grounded && Input.GetButton("Jump")){
             grounded = false;
             playerRb.AddForce(0, jumpForce, 0);
         }
@@ -22,8 +34,8 @@ public class PlayerAction : MonoBehaviour {
         }
     }
 
-    void OnCollisionEnter(Collision collisionObject){
-        if(collisionObject.gameObject != gameObject){
+    void OnCollisionStay(Collision collisionObject){
+        if(playerTf.position.y - playerTf.lossyScale.y / 2 >= collisionObject.gameObject.transform.position.y + collisionObject.gameObject.transform.lossyScale.y / 2){
             grounded = true;
         }
     }
